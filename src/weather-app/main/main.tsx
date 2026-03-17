e70e31edf84a973b0698ed3ffe93c5da1d2eb93e
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2026-03-17 15:37:51 -0700

    fix(weather-app): mount WeatherApp in main entrypoint

--- src/weather-app/main/main.tsx
+++ src/weather-app/main/main.tsx
@@ -3,3 +3,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../global.css";
+import { WeatherApp } from "./WeatherApp";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <WeatherApp />
+  </StrictMode>,
+);
